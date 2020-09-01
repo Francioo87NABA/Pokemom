1f9150175d8271fedf8e51ded46280215ad313b7
Francioo87NABA <francesco_palma@studenti.naba.it>
2020-09-01 16:36:47 +0200

    i primi passi

---
+++ Pokemon/New Unity Project/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+    public BattleState state;
+
+    public GameObject enemyPrefab;
+    public GameObject playerPrefab;
+
+    Unit enemyUnit;
+    Unit playerUnit;
+
+    public Transform enemyBattleStation;
+    public Transform playerBattleStation;
+
+    public BattleHUD enemyHUD;
+    public BattleHUD playerHUD;
+
+    public Text dialogueText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetUpBattle();
+
+    }
+
+    void SetUpBattle()
+    {
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+        
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<Unit>();
+
+        dialogueText.text = "E' apparso un " + enemyUnit.unitName + " selvatico";
+
+        enemyHUD.SetHUD(enemyUnit);
+        playerHUD.SetHUD(playerUnit);
+    }
+}
